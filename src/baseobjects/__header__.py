c12823f66ecf8549aef10273d39e3f8314d41571
Anthony Fong <FongAnthonyM@gmail.com>
2021-10-04 14:17:36 -0700

    Added new header file

---
+++ src/baseobjects/__header__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" __header__.py
+Description: Information about this package.
+"""
+# Header #
+__author__ = "Anthony Fong"
+__credits__ = ["Anthony Fong"]
+__maintainer__ = "Anthony Fong"
+__email__ = ""
+
+__copyright__ = "Copyright 2021, Anthony Fong"
+__license__ = "MIT"
+
+__version__ = "1.4.4"
+__status__ = "Prototype"
